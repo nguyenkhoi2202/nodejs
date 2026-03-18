79d42b688caacb5272f32516e438ce1c5502964e
nguyenkhoi2202 <khoitn2202@gmail.com>
2026-03-18 23:10:41 +0700

    fix1

--- api/test.js
+++ api/test.js
@@ -1,3 +1,19 @@
-module.exports = (req, res) => {
-    res.status(200).json({ ok: true });
-  };
+module.exports = async (req, res) => {
+    try {
+        const data = await peopleService.login();
+    
+        res.json({
+          success: true,
+          stoken: data.message,
+          raw: data
+        });
+    
+      } catch (err) {
+        console.error(err.response?.data || err.message);
+    
+        res.status(500).json({
+          success: false,
+          error: err.message
+        });
+      }
+};
