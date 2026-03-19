8762305ec15be0241e6ccd93a19dd79ea6ed8f6e
khoitn1 <khoitn1@hdbank.com.vn>
2026-03-19 08:27:48 +0700

    áa

---
+++ api/login.js
@@ -0,0 +1,30 @@
+const peopleService = require("../src/services/people.service");
+
+module.exports = async (req, res) => {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  try {
+    const data = await peopleService.login();
+
+    res.json({
+      success: true,
+      stoken: data.message,
+      raw: data
+    });
+
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+
+    res.status(500).json({
+      success: false,
+      error: err.message
+    });
+  }
+};
